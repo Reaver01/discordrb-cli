1840d14e85cbc5d685a2c2e744c9ab20baa0551c
Reaver01 <jawmac@gmail.com>
2018-05-08 19:27:56 +0000

    change styling of messages

--- src/modules/io/parse_message.rb
+++ src/modules/io/parse_message.rb
@@ -4,32 +4,33 @@ def parse_message(message)
   # Mentions
   if message.mentions.include? Bot::BOT.user(ENV['OWNER'].to_i)
     $last_mention = message.channel.id
-    puts "[#{time}][#{"@#{message.user.name}##{message.user.tag}".red}|#{message.channel.id}] "\
-         "#{message.content.yellow}"
+    puts "[#{time}][<@#{message.user.id}>][#{message.channel.id}][@#{message.user.name}##{message.user.tag}]".red
+    puts message.content.yellow
     puts message.attachments.first.proxy_url unless message.attachments.first.nil?
 
   # Channel 1 Messages
   elsif message.channel.id == $channel1
-    puts "[#{time}][#{"@#{message.user.name}##{message.user.tag}".blue}] #{message.content.light_cyan}"
+    puts "[#{time}][<@#{message.user.id}>][#{message.channel.id}][@#{message.user.name}##{message.user.tag}]".light_magenta
+    puts message.content.light_cyan
     puts message.attachments.first.proxy_url unless message.attachments.first.nil?
 
   # Channel 2 Messages
   elsif message.channel.id == $channel2
-    puts "[#{time}][#{"@#{message.user.name}##{message.user.tag}".light_green}|"\
-         "#{message.channel.id}] #{message.content.magenta}"
+    puts "[#{time}][<@#{message.user.id}>][#{message.channel.id}][@#{message.user.name}##{message.user.tag}]".light_green
+    puts message.content.magenta
     puts message.attachments.first.proxy_url unless message.attachments.first.nil?
 
   # Channel 3 Messages
   elsif message.channel.id == $channel3
-    puts "[#{time}][#{"@#{message.user.name}##{message.user.tag}".yellow}|"\
-         "#{message.channel.id}] #{message.content.light_green}"
+    puts "[#{time}][<@#{message.user.id}>][#{message.channel.id}][@#{message.user.name}##{message.user.tag}]".yellow
+    puts message.content.light_red
     puts message.attachments.first.proxy_url unless message.attachments.first.nil?
 
   # Direct Messages
   elsif message.channel.type == 1
     $last_mention = message.channel.id
-    puts "[#{time}][#{"@#{message.user.name}##{message.user.tag}".red}|#{message.channel.id}] "\
-         "#{message.content.green}"
+    puts "[#{time}][<@#{message.user.id}>][#{message.channel.id}][@#{message.user.name}##{message.user.tag}]".red
+    puts message.content.green
     puts message.attachments.first.proxy_url unless message.attachments.first.nil?
   end
 end
