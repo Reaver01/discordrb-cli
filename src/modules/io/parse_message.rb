32d74117ee3096628506f3437bed831766c0cf6d
Reaver01 <jawmac@gmail.com>
2018-05-08 18:16:52 +0000

    display link to attachment for attached things

--- src/modules/io/parse_message.rb
+++ src/modules/io/parse_message.rb
@@ -6,25 +6,30 @@ def parse_message(message)
     $last_mention = message.channel.id
     puts "[#{time}][#{"@#{message.user.name}##{message.user.tag}".red}|#{message.channel.id}] "\
          "#{message.content.yellow}"
+    puts message.attachments.first.proxy_url unless message.attachments.first.nil?
 
   # Channel 1 Messages
   elsif message.channel.id == $channel1
     puts "[#{time}][#{"@#{message.user.name}##{message.user.tag}".blue}] #{message.content.light_cyan}"
+    puts message.attachments.first.proxy_url unless message.attachments.first.nil?
 
   # Channel 2 Messages
   elsif message.channel.id == $channel2
     puts "[#{time}][#{"@#{message.user.name}##{message.user.tag}".light_green}|"\
          "#{message.channel.id}] #{message.content.magenta}"
+    puts message.attachments.first.proxy_url unless message.attachments.first.nil?
 
   # Channel 3 Messages
   elsif message.channel.id == $channel3
     puts "[#{time}][#{"@#{message.user.name}##{message.user.tag}".yellow}|"\
          "#{message.channel.id}] #{message.content.light_green}"
+    puts message.attachments.first.proxy_url unless message.attachments.first.nil?
 
   # Direct Messages
   elsif message.channel.type == 1
     $last_mention = message.channel.id
     puts "[#{time}][#{"@#{message.user.name}##{message.user.tag}".red}|#{message.channel.id}] "\
          "#{message.content.green}"
+    puts message.attachments.first.proxy_url unless message.attachments.first.nil?
   end
 end
