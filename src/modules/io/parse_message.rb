2e4dcb4090dbd396a425c267120b1aa2c1735bd9
Reaver01 <jawmac@gmail.com>
2018-05-11 20:53:01 +0000

    simplify code

--- src/modules/io/parse_message.rb
+++ src/modules/io/parse_message.rb
@@ -1,48 +1,27 @@
 module Bot
   extend Bot
   def parse_message(message)
+    
+    color = Array.new
+    color = if message.mentions.include? BOT.user(ENV['OWNER'].to_i)
+            ['green', 'red']
+          elsif message.channel.id == CHANNELS[0]
+            [COLORS[0][0], COLORS[0][1]]
+          elsif message.channel.id == CHANNELS[1]
+            [COLORS[1][0], COLORS[1][1]]
+          elsif message.channel.id == CHANNELS[2]
+            [COLORS[2][0], COLORS[2][1]]
+          elsif message.channel.type == 1
+            ['red', 'green']
+          end
 
-    # Mentions
-    if message.mentions.include? BOT.user(ENV['OWNER'].to_i)
-      CHANNELS[3] = message.channel.id
-      info = 'green'
-      text = 'red'
-      display = true
-
-    # Channel 1 Messages
-    elsif message.channel.id == CHANNELS[0]
-      info = COLORS[0][0]
-      text = COLORS[0][1]
-      display = true
-
-    # Channel 2 Messages
-    elsif message.channel.id == CHANNELS[1]
-      info = COLORS[1][0]
-      text = COLORS[1][1]
-      display = true
-
-    # Channel 3 Messages
-    elsif message.channel.id == CHANNELS[2]
-      info = COLORS[2][0]
-      text = COLORS[2][1]
-      display = true
-
-    # Direct Messages
-    elsif message.channel.type == 1
-      CHANNELS[3] = message.channel.id
-      info = 'red'
-      text = 'green'
-      display = true
-    else
-
-      display = false
-    end
+    CHANNELS[3] = message.channel.id if message.mentions.include? BOT.user(ENV['OWNER'].to_i) || message.channel.type == 1
 
     # display messages
-    if display
+    if color.any?
       begin
-        puts "[#{message.timestamp.strftime('%I:%M%p')}][<@#{message.user.id}>][#{message.channel.id}][@#{message.user.name}##{message.user.tag}]".colorize(:"#{info}")
-        puts message.content.colorize(:"#{text}")
+        puts "[#{message.timestamp.strftime('%I:%M%p')}][<@#{message.user.id}>][#{message.channel.id}][@#{message.user.name}##{message.user.tag}]".colorize(:"#{color[0]}")
+        puts message.content.colorize(:"#{color[1]}")
         puts message.attachments.first.proxy_url if message.attachments.first
       rescue error
         puts error
